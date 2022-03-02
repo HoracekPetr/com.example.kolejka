dea223c55c333667d30294327b8b9437ec1184b4
HoracekPetr <petr.horacekstu@gmail.com>
2022-03-02 13:15:07 +0100

    zmena heroku

--- src/main/kotlin/com/example/util/Constants.kt
+++ src/main/kotlin/com/example/util/Constants.kt
@@ -10,7 +10,7 @@ object Constants {
     const val PROFILE_PIC_URL = "https://kolejka-app.herokuapp.com/profile_pictures"
 
     //const val POST_PIC_PATH = "build/resources/main/static/post_pictures"
-    const val POST_PIC_PATH = "./build/resources/main/static/post_pictures"
+    const val POST_PIC_PATH = "static/post_pictures"
     const val POST_PIC_URL = "https://kolejka-app.herokuapp.com/post_pictures"
 
     const val DEFAULT_AVATAR_URL = "https://kolejka-app.herokuapp.com/profile_pictures/default_profile_pic.png"
