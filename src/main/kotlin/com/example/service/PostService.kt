7b575a37fc8f57cb89c230971bce33ffb6c81bfc
HoracekPetr <petr.horacekstu@gmail.com>
2022-04-12 16:24:17 +0200

    Notification count se vytvoří při vytvoření uživatele

--- src/main/kotlin/com/example/service/PostService.kt
+++ src/main/kotlin/com/example/service/PostService.kt
@@ -62,7 +62,7 @@ class PostService(
             return EditPostValidation.TitleTooLong
         }
 
-        if(updatePostRequest.limit < (postRepository.getPostById(updatePostRequest.postId)?.members?.size ?: 0)){
+        if(updatePostRequest.limit < ((postRepository.getPostById(updatePostRequest.postId)?.members?.size ?: 0) + 1)){
             return EditPostValidation.LimitCantBeLower
         }
 
